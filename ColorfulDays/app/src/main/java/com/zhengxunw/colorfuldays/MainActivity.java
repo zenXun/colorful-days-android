38d6d0dbde8ca2f1114bf4057db9cf324b6e8cfa
zhengxunw <zhengxunw@vmware.com>
2018-03-14 08:10:22 -0700

    Finished Home view layout with ConstrantLayout.

---
+++ ColorfulDays/app/src/main/java/com/zhengxunw/colorfuldays/MainActivity.java
@@ -0,0 +1,51 @@
+package com.zhengxunw.colorfuldays;
+
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextDate;
+    private static final DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy");
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_today:
+                    displayCurrentDate();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextDate = (TextView) findViewById(R.id.today_date);
+        displayCurrentDate();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void displayCurrentDate() {
+        Date currentTime = Calendar.getInstance().getTime();
+        mTextDate.setText(dateFormat.format(currentTime));
+    }
+
+}
