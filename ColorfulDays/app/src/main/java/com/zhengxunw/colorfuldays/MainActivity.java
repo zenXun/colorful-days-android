bc9ca4d706141e1c36aa1dc07920215c28149182
zhengxunw <zhengxunw@vmware.com>
2018-03-14 09:49:14 -0700

    Finished Home/Stats/Color pages with Fragments.

--- ColorfulDays/app/src/main/java/com/zhengxunw/colorfuldays/MainActivity.java
+++ ColorfulDays/app/src/main/java/com/zhengxunw/colorfuldays/MainActivity.java
@@ -6,6 +6,9 @@ import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -15,6 +18,8 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextDate;
+    private FragmentManager fragmentManager;
+    private Fragment homeFragment, statsFragment, colorFragment;
     private static final DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy");
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -22,9 +27,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_today:
-                    displayCurrentDate();
+                    transaction.replace(R.id.content, homeFragment).commit();
+                    return true;
+                case R.id.navigation_single_stats:
+                    transaction.replace(R.id.content, statsFragment).commit();
+                    return true;
+                case R.id.navigation_total_stats:
+                    transaction.replace(R.id.content, colorFragment).commit();
                     return true;
             }
             return false;
@@ -35,12 +47,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_today);
 
-        mTextDate = (TextView) findViewById(R.id.today_date);
-        displayCurrentDate();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        fragmentManager = getSupportFragmentManager();
+        homeFragment = new HomeFragment();
+        statsFragment = new StatsFragment();
+        colorFragment = new ColorFragment();
+        fragmentManager.beginTransaction().replace(R.id.content, homeFragment).commit();
     }
 
     private void displayCurrentDate() {
